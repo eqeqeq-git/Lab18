ce53998b58f3b6bfc4c33dbbd77242fef5447d76
eqeqeq-git <pakinwakaek@gmail.com>
2026-02-13 04:26:31 +0700

    Lab18

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,21 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2) {
+    double left = max(r1.x, r2.x);
+    double right = min(r1.x + r1.w, r2.x + r2.w);
+    double overlap_w = right - left;
+    double top = min(r1.y, r2.y);
+    double bottom = max(r1.y - r1.h, r2.y - r2.h);
+    double overlap_h = top - bottom;
 
-
-
+    if (overlap_w > 0 && overlap_h > 0) {
+        return overlap_w * overlap_h;
+    }
+    return 0;
 }
--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *r1, Rect *r2) {
+    double left = max(r1->x, r2->x);
+    double right = min(r1->x + r1->w, r2->x + r2->w);
+    double overlap_w = right - left;
+    double top = min(r1->y, r2->y);
+    double bottom = max(r1->y - r1->h, r2->y - r2->h);
+    double overlap_h = top - bottom;
 
-
+    if (overlap_w > 0 && overlap_h > 0) {
+        return overlap_w * overlap_h;
+    }
+    return 0;
 
 }
